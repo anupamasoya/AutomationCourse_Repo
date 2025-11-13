d41db2dffa92b7f9c20d1240246103dba01d2ea1
Anupama S <Anupama S@LAPTOP-UCLTDI57>
2025-11-13 20:47:54 +0530

    HandlingActions

--- AutomationCourse/src/test/java/seleniumbasics/HandlingFrame.java
+++ AutomationCourse/src/test/java/seleniumbasics/HandlingFrame.java
@@ -10,10 +10,18 @@ public class HandlingFrame extends Base {
 		driver.navigate().to("https://demoqa.com/frames");
 		List<WebElement> totalframe = driver.findElements(By.tagName("iframe"));
 		System.out.println(totalframe.size());
+		
+		WebElement frame1=driver.findElement(By.id("frame1"));
+		driver.switchTo().frame(frame1);
+		
+		WebElement framename =driver.findElement(By.id("sampleHeading"));
+		System.out.println(framename.getText());
+		
+		driver.switchTo().defaultContent();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubl
 		HandlingFrame frame = new HandlingFrame();
 		frame.initializeBrowser();
 		frame.verifyframe();
