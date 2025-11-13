a9ceaaa3bd47d8cc341d0352e4517b8803be2393
Anupama S <Anupama S@LAPTOP-UCLTDI57>
2025-11-13 21:47:17 +0530

    Commit

--- AutomationCourse/src/test/java/seleniumbasics/HandlingActions.java
+++ AutomationCourse/src/test/java/seleniumbasics/HandlingActions.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class HandlingActions extends Base {
-	public void verifyrightclick() {
+	public void verifyRightClick() {
 		WebElement others = driver.findElement(By.id("others"));
 		Actions action = new Actions(driver);
 		action.contextClick(others).build().perform();
